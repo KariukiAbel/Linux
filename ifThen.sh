3b69eaf7d6c16ecb1c16221dcfddc9d481169c53
KariukiAbel <abelmuriithi@gmail.com>
2020-03-10 17:53:13 +0300

    Learned how to use variables and comments in shell scripts

--- ifThen.sh
+++ ifThen.sh
@@ -4,3 +4,11 @@ if [ $STRING = "Friday" ] ; then
 else
 	echo "Will Friday ever get here?"
 fi
+#using if then for comparison in numbers
+
+VALUE=10
+if [ $VALUE -eq 10 ] ; then
+	echo "The numbers are equal"
+else
+	echo "The number is not equal to 10."
+fi
