89034533b21b4783eaf1e21e7f8dc8eea7026d70
KariukiAbel <abelmuriithi@gmail.com>
2020-03-12 23:08:51 +0300

    Doing arithmetic operations

---
+++ arithmeticOperations.sh
@@ -0,0 +1,11 @@
+echo "Enter first number: "
+read num1
+echo "Enter second number: "
+read num2
+echo $((num1 + num2))
+echo $((num1 - num2))
+echo $((num1 * num2))
+echo $((num1 / num2))
+
+
+#echo `expr $num1 = $num2`
