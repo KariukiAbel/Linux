a6e2793ef1908b794832308a70a9e4b2ae0d5528
KariukiAbel <abelmuriithi@gmail.com>
2020-03-14 21:25:17 +0300

    use elif to better the results

--- triangleTypes.sh
+++ triangleTypes.sh
@@ -2,11 +2,9 @@ read x
 read y
 read z
 
-if [[ $x == $y && $y == $z ]]; then
+if [[ $x -eq $y && $y -eq $z ]]; then
 	echo "EQUILATERAL"
-fi
-
-if [[ $x != $y && $y != $z ]]; then
+elif [[ $x != $y && $y != $z ]]; then
 	echo "SCALENE"
 else
 	echo "ISOSCELES"
